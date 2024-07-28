27d257ef0e25b0fe6962b10bb5fc130709b719ed
JeanPierre229 <dodohoundealo@gmail.com>
2024-07-28 23:55:27 +0100

    SAEI-MANAGER

--- payerCommencer.php
+++ payerCommencer.php
@@ -1,6 +1,6 @@
 <section class="my-4 bg-dark mx-3 text-light p-5 animated-text-y">
-    <h3 class="text-center">LOREM IPSUM DOLOR SIT AMET.</h3>
-    <h5 class="text-center my-4">LOREM IPSUM DOLOR SIT AMET. LOREM IPSUM DOLOR SIT AMET. LOREM IPSUM DOLOR SIT AMET. LOREM IPSUM DOLOR SIT AMET. LOREM IPSUM DOLOR SIT AMET.</h5>
+    <h3 class="text-center">PRENEZ UN ABONNEMENT DES MAINTENANT</h3>
+    <h5 class="text-center my-4">Abonnez-vous à SAEI-MANAGER pour optimiser la gestion de vos structures et accéder à des outils performants, un support dédié, et des ressources exclusives.</h5>
     <p class="text-center">
         <a href="tarifs.php">
             <button class="btn btn-orange rounded-0 px-5 py-2"><strong>COMMENCER</strong></button>
